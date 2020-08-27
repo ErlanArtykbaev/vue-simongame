d4462e2626bc12dc2e332f4ca03408f5286c2081
erlan artykbaev <erik.2405@mail.ru>
2020-08-28 00:29:24 +0600

    base scelet is done

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,9 +1,32 @@
 <template>
-  <div>{{msg}}</div>
+  <div class="simon">
+    <div>Simon game</div>
+    <div class="game">
+      <div class="buttons">
+        <div class="button"></div>
+        <div class="button"></div>
+        <div class="button"></div>
+        <div class="button"></div>
+      </div>
+      <div class="controlers">
+        <button v-on:click="start">start</button>
+        <input type="radio" name="light" id="light">
+        <input type="radio" name="norman" id="normal">
+        <input type="radio" name="hard" id="hard">
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import '@/assets/styles/style.scss'
+
 export default {
-  props: ['msg']
+  props: ['msg'],
+  methods: {
+    start(){
+      console.log('started')
+    }
+  }
 }
 </script>
