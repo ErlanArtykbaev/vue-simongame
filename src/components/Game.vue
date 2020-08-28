cfaf27fc8511baf004ba16d7779c4c3f4262c84c
erlan artykbaev <erik.2405@mail.ru>
2020-08-28 16:29:56 +0600

    controllers are ready

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -12,11 +12,17 @@
           <div class="button4 mesure"></div>
         </div>
       </div>
-      <div class="controlers">
+      <div class="controllers">
         <button v-on:click="start">start</button>
-        <input type="radio" name="light" id="light">
-        <input type="radio" name="norman" id="normal">
-        <input type="radio" name="hard" id="hard">
+        <div class="round">Round: {{round}}</div>
+        <div class="radio-buttons">
+          <input type="radio" value="light" id="light" checked="true" v-model="complexity">
+          <label for="light">Легкий</label>
+          <input type="radio" value="norman" id="normal" v-model="complexity">
+          <label for="normal">Нормальный</label>
+          <input type="radio" value="hard" id="hard" v-model="complexity">
+          <label for="hard">Сложный</label>
+        </div>
       </div>
     </div>
   </div>
@@ -27,10 +33,16 @@ import '@/assets/styles/style.scss'
 
 export default {
   props: ['msg'],
-  methods: {
-    start(){
-      console.log('started')
+  data(){
+    return{
+      round: 1,
+      complexity: ''
+      }
+    },
+    methods: {
+      start(){
+        console.log('started')
+      }
     }
   }
-}
-</script>
+  </script>
